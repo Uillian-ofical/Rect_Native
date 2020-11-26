82ee1320ecb9d9544f1314ec36cb129a39620317
Uillian-ofical <uillian.gvalerio@gmail.com>
2020-11-26 10:06:18 -0200

    subindo correçoes

--- src/pages/Login/Home/index.js
+++ src/pages/Login/Home/index.js
@@ -1,24 +1,54 @@
-import React, {useState} from 'react';
-import { Text, View} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+mport React, { useState, useEffect } from 'react';
+import { Text, View, FlatList } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import ItemEvento from '../../components/itemEvento';
+
 
 const Home = () => {
 
     const [token, setToken] = useState('');
+    const [eventos, setEventos] = useState([]);
 
-    const getToken = async () => {
-      setToken(await AsyncStorage.getItem('@jwt'));
+    // const getToken = async () => {
+    //     setToken(await AsyncStorage.getItem('@jwt'));
+    // }
+
+    
+    // useEffect(()=>{
+    //     getToken();
+
+    // }, [])
+
+    useEffect(() => {
+        listarEventos();
+    }, []); 
+
+    const listarEventos = () => {
+        fetch('http://192.168.3.55:5000/api/eventos')
+            .then(response => response.json())
+            .then(data => {
+                setEventos(data.data)
+                console.log(data.data);
+            })
+            .catch(err => console.error(err));
     }
 
-    useEffect(()=>{
-      getToken();
-    }, [])
-
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home</Text>
-      </View>
+    const renderItem = ({ item }) => (
+        <ItemEvento nome={item.nome} imagem={item.urlImagem} link={item.link} />
       );
-    }
+
+    return(
+        <View>
+            <Text>HOME</Text>
+            {/* <Text>{token}</Text> */}
+            <FlatList
+                data={eventos}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
+        </View>
+    )
+}
 
 export default Home;
